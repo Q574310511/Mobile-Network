5d7bec9e582891b7d64af19cfa84fe0a940cba44
li <3141198725@qq.com>
2021-04-27 15:29:08 +0800

    tete

---
+++ lib/5120184961/login_02.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class login_02 extends StatelessWidget{
+  Widget build(BuildContext context){
+    return Center(
+        child:Text('02\'s homepage',
+          textDirection: TextDirection.ltr,)
+    );
+  }
+}
