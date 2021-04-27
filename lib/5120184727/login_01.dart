d032c761378d8e0c92c4c2b077fe7ab2d8906585
57431 <574310511@qq.com>
2021-04-27 17:02:50 +0800

    提交测试

--- lib/5120184727/login_01.dart
+++ lib/5120184727/login_01.dart
@@ -1,10 +1,80 @@
 import 'package:flutter/material.dart';
+class login_01 extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => login_01State();
+}
+class login_01State extends State<login_01>{
+  TextEditingController phoneController = TextEditingController();
+  TextEditingController passController = TextEditingController();
 
-class login_01 extends StatelessWidget{
+  @override
   Widget build(BuildContext context){
-    return Center(
-      child:Text('01\'s homepage',
-      textDirection: TextDirection.ltr,)
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('用户登录界面'),
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children:<Widget>[
+          TextField(
+            controller: phoneController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(
+              border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.all(10.0),
+              icon: Icon(Icons.person),
+              hintText: '请输入手机号',
+              labelText: '账号',
+            ),
+            autofocus: false,
+          ),
+          TextField(
+              controller: passController,
+              keyboardType: TextInputType.text,
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.all(10.0),
+                icon: Icon(Icons.lock),
+                labelText: '请输入密码',
+              ),
+              obscureText: true),
+          ElevatedButton(
+            onPressed: _login,
+            child: Text('登录'),
+          ),
+        ],
+      ),
     );
   }
+  void _login() {
+    print({'phone': phoneController.text, 'password': passController.text});
+    if (phoneController.text.length != 11) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('手机号码格式不对'),
+          ));
+    } else if (passController.text.length == 0) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('请填写密码'),
+          ));
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text('登录成功'),
+          ));
+      phoneController.clear();
+    }
+  }
+
+  void onTextClear() {
+    setState(() {
+      phoneController.clear();
+      passController.clear();
+    });
+  }
 }
+
